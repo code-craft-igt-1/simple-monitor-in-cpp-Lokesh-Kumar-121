5c4bf16c28e1fa62472f08294361759c0b0efc3c
Lokesh Kumar <Lokesh.Kumar@philips.com>
2024-09-10 00:57:09 +0530

    multi language and warnings added

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -3,8 +3,16 @@
 #include "gtest/gtest.h"
 
 TEST(Monitor, NotOkWhenAnyVitalIsOffRange) {
-  ASSERT_FALSE(vitalsOk(99, 102, 95));
   ASSERT_FALSE(vitalsOk(103, 70, 95));
-  ASSERT_FALSE(vitalsOk(99, 70, 70));
-  ASSERT_TRUE(vitalsOk(98.1, 70, 98));
+  ASSERT_TRUE(vitalsOk(102, 70, 95));
+  ASSERT_TRUE(vitalsOk(96, 70, 95));
+
+  ASSERT_FALSE(vitalsOk(99, 110, 95));
+  ASSERT_TRUE(vitalsOk(99, 99, 95));
+  ASSERT_TRUE(vitalsOk(99, 60.5, 95));
+
+  ASSERT_FALSE(vitalsOk(99, 70, 80));
+  ASSERT_TRUE(vitalsOk(99, 70, 91));
+
+  ASSERT_TRUE(vitalsOk(99, 70, 98));
 }
